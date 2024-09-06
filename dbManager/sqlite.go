c5f92c6446cf4df51cd1f3d11b2f8f3bb176d230
Yoel T Laptop <me.trcue@gmail.com>
2024-09-05 21:44:56 -0400

    update

--- dbManager/sqlite.go
+++ dbManager/sqlite.go
@@ -16,8 +16,28 @@ func checkErr(err error) {
 
 // Connect to database
 func makedb() {
+	fmt.Println(sql.Drivers())
 	db, err := sql.Open("sqlite3", "./permits.db")
 	checkErr(err)
 	defer db.Close()
 	fmt.Println(sqlited.Version())
+
+	sqlStmt := `
+	CREATE TABLE permits ( id INTEGER PRIMARY KEY, permit INTEGER);
+	`
+	_, err = db.Exec(sqlStmt)
+	if err != nil {
+		log.Printf("%q: %s\n", err, sqlStmt)
+		return
+	}
+	sql2 := `
+	INSERT INTO permits VALUES 1;
+	`
+	x, err := db.Exec(sql2)
+	if err != nil {
+		log.Printf("%q: %s\n", err, sqlStmt)
+		return
+	}
+	fmt.Println(x)
+
 }
